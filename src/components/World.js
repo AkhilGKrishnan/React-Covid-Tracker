be88a93fd98fc67fc3ebd51d0f0ba25b4d82b85f
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-01-12 22:13:13 +0530

    class component removed and hooks in world

--- src/components/World.js
+++ src/components/World.js
@@ -1,53 +1,47 @@
-import React, { Component} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-class World extends Component {
-  constructor(){
-    super();
-    this.state = {
-      data: []
-    }
-  }
-  componentDidMount(){ 
-    axios.get("https://corona.lmao.ninja/v2/countries").then(response=>{
-      this.setState({data: response.data});
+function World() {
+  const [data, setData] = useState([]);
+  useEffect(()=>{ 
+    axios.get("https://corona.lmao.ninja/v2/countries").then((response)=>{
+      setData(response.data);
     });
-  }
-  render(){
-    return(
-      <div className="row">
-      
-        <div className="col-md-12">
-          <table className="table table-primary table-bordered table-striped">
-            <thead>
-              <tr>
-                <td>Country</td>
-                <td>Total Cases</td>
-                <td>Recovered</td>
-                <td>Death</td>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                this.state.data.map((item,key)=>{
-                  return(
-                    <tr>
-                      <td>{item.country}
-                        <img alt="" style={ {width:'64px', marginLeft: '10px'} } src={item.countryInfo.flag}/>
-                      </td>
-                      <td>{item.cases}</td>
-                      <td>{item.recovered}</td>
-                      <td>{item.deaths}</td>
-                    </tr>
-                  )
-                })
-              }
-            </tbody>
-          </table>
-        </div>
+  })
+
+  return(
+    <div className="row">
+    
+      <div className="col-md-12">
+        <table className="table table-primary table-bordered table-striped">
+          <thead>
+            <tr>
+              <td>Country</td>
+              <td>Total Cases</td>
+              <td>Recovered</td>
+              <td>Death</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.map((item,key)=>{
+                return(
+                  <tr>
+                    <td>{item.country}
+                      <img alt="" style={ {width:'64px', marginLeft: '10px'} } src={item.countryInfo.flag}/>
+                    </td>
+                    <td>{item.cases}</td>
+                    <td>{item.recovered}</td>
+                    <td>{item.deaths}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
       </div>
-    )
-  }
+    </div>
+  );
 }
 
 export default World;
