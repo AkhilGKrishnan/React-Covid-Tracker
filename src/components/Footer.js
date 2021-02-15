8c1ae118c373c7bae7784ca88a9597e5cdda2331
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-02-15 23:44:16 +0530

    unwanted files removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component{
   render() {
     return(
       <div className="footer-copyright text-center py-3">
-         Developed By <a href="https://akhilgkrishnan.me">Akhil G Krishnan</a>
+         Developed By <a href="https://akhilgkrishnan.me" target="_blank">Akhil G Krishnan</a>
       </div>
     )
   }
